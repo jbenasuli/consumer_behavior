55b91cba1985e13f6548336fa5de19ae2a5cbb54
James Benasuli <jamiebenasuli@gmail.com>
2021-05-02 18:31:58 -0400

    populate segment table queries

--- dev/database/schemas-final/segment-analysis-tables.sql
+++ dev/database/schemas-final/segment-analysis-tables.sql
@@ -1,4 +1,9 @@
--- music_segment table already created
+-- Static Segment Data for Apriori Analysis
+-- Only verified purchase reviews
+-- Columns = customer_id, review_id and product_id
+-- PK = review_id
+
+-- create music_segment table
 CREATE TABLE "music_segment" (
     "customer_id" int,
     "music" int,
@@ -7,19 +12,34 @@ CREATE TABLE "music_segment" (
      )
 );
 
+-- create video_games_segment table
+CREATE TABLE "video_games_segment" (
+    "customer_id" int,
+    "music" int,
+    CONSTRAINT "pk_video_games_segment" PRIMARY KEY (
+        "customer_id"
+     )
+);
 
+-- create videos_segment table
+CREATE TABLE "videos_segment" (
+    "customer_id" int,
+    "music" int,
+    CONSTRAINT "pk_videos_segment" PRIMARY KEY (
+        "customer_id"
+     )
+);
 
+-- create watches_segment table
+CREATE TABLE "watches_segment" (
+    "customer_id" int,
+    "music" int,
+    CONSTRAINT "pk_watches_segment" PRIMARY KEY (
+        "customer_id"
+     )
+);
 
--- to create
-
--- video_games_segment
-
--- videos_segment
-
--- watches_segment
-
--- furniture_segment
-
+-- create furniture_segment table
 CREATE TABLE "furniture_segment" (
     "customer_id" int,
     "music" int,
@@ -27,6 +47,30 @@ CREATE TABLE "furniture_segment" (
         "customer_id"
      )
 );
--- office_products_segment
 
--- personal_care_segment
+-- create office_products_segment table
+CREATE TABLE "office_products_segment" (
+    "customer_id" int,
+    "music" int,
+    CONSTRAINT "pk_office_products_segment" PRIMARY KEY (
+        "customer_id"
+     )
+);
+
+-- create personal_care_appliances_segment table
+CREATE TABLE "personal_care_appliances_segment" (
+    "customer_id" int,
+    "music" int,
+    CONSTRAINT "pk_personal_care_appliances_segment" PRIMARY KEY (
+        "customer_id"
+     )
+);
+
+-- create apparel_segment table
+CREATE TABLE "apparel_segment" (
+    "customer_id" int,
+    "music" int,
+    CONSTRAINT "pk_apparel_segment" PRIMARY KEY (
+        "customer_id"
+     )
+);
