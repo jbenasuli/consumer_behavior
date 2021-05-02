d954982e626a85d66aba2be59d08afcebedbd19a
James Benasuli <jamiebenasuli@gmail.com>
2021-05-02 15:25:24 -0400

    add final schema folder and files

---
+++ dev/database/schemas-final/apriori-analysis-tables.sql
@@ -0,0 +1,23 @@
+-- music_apriori table already created
+CREATE TABLE "music_apriori" (
+    "customer_id" int,
+	"review_id" varchar,
+    "product_id" varchar,
+    CONSTRAINT "pk_music_apriori" PRIMARY KEY (
+        "review_id"
+     )
+);
+
+-- to create
+
+-- video_games_apriori
+
+-- videos_apriori
+
+-- watches_appriori
+
+-- furniture_appriori
+
+-- office_products_apriori
+
+-- personal_care_appliances
---
+++ dev/database/schemas-final/segment-analysis-tables.sql
@@ -0,0 +1,32 @@
+-- music_segment table already created
+CREATE TABLE "music_segment" (
+    "customer_id" int,
+    "music" int,
+    CONSTRAINT "pk_music_segment" PRIMARY KEY (
+        "customer_id"
+     )
+);
+
+
+
+
+-- to create
+
+-- video_games_segment
+
+-- videos_segment
+
+-- watches_segment
+
+-- furniture_segment
+
+CREATE TABLE "furniture_segment" (
+    "customer_id" int,
+    "music" int,
+    CONSTRAINT "pk_furniture_segment" PRIMARY KEY (
+        "customer_id"
+     )
+);
+-- office_products_segment
+
+-- personal_care_segment
