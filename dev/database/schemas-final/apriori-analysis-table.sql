40ae780920e2b2563fc4973473b12ec5dcf20e2c
James Benasuli <jamiebenasuli@gmail.com>
2021-05-06 03:18:01 -0400

    add apriori table generation queries

---
+++ dev/database/schemas-final/apriori-analysis-table.sql
@@ -0,0 +1,133 @@
+-- Apriori table creation
+
+-- Step 1: create new table for the apriori merge
+-- CREATE TABLE "apriori_analysis" (
+--     "customer_id" int,
+--     "review_id" varchar,
+-- 	"product_id" varchar,
+-- 	"quantity" int,
+--     CONSTRAINT "pk_apriori_analysis" PRIMARY KEY (
+--         "review_id"
+--      )
+-- );
+
+CREATE TABLE "apriori_analysis" (
+    "customer_id" int,
+	"product_id" varchar,
+	"quantity" int);
+
+-- check table
+SELECT * FROM apriori_analysis;
+
+-- Step 2a: add quantity column with value of 1 to each segment_apriori table
+ALTER TABLE table_name
+	ADD quantity INT NOT NULL DEFAULT 1;
+
+-- Step 2b: append each segment_apriori table to the new apriori_analysis table
+-- Use nested filter query to select only Product Ids with a count > 50
+
+WHERE product_id IN (
+	SELECT product_id
+	FROM (
+		SELECT product_id,
+		COUNT(product_id)  as product_counts
+		FROM apparel_apriori
+		GROUP BY product_id
+		HAVING COUNT(product_id) >50) as filtered_list);
+
+
+-- apparel segment
+-- add quantity column
+ALTER TABLE apparel_apriori
+	ADD quantity INT NOT NULL DEFAULT 1;
+
+-- append filtered apparel data to apriori_analysis table
+-- count threshold = 50 yields 601,375 results
+-- count threshold = 100 yields 314,501 results
+-- count threshold = 200 yields 145,686 results 83038
+-- count threshold = 200 yields 83,038 results 
+
+INSERT INTO apriori_analysis (customer_id, product_id, quantity)
+	SELECT customer_id, product_id, quantity
+	FROM apparel_apriori
+	WHERE product_id IN (
+		SELECT product_id
+		FROM (
+			SELECT product_id,
+			COUNT(product_id)  as apparel_counts
+			FROM apparel_apriori
+			GROUP BY product_id
+			HAVING COUNT(product_id) >200) as filtered_list);
+
+-- check for successful import
+SELECT * FROM apriori_analysis LIMIT 10;
+
+-- furniture segment
+-- add quantity column
+ALTER TABLE furniture_apriori
+	ADD quantity INT NOT NULL DEFAULT 1;
+
+-- append filtered furniture data to apriori_analysis table
+-- count threshold = 50 yields xxx,xxx results
+-- count threshold = 100 yields xx,xxx results
+INSERT INTO apriori_analysis (customer_id, product_id, quantity)
+	SELECT customer_id, product_id, quantity
+	FROM furniture_apriori
+	WHERE product_id IN (
+		SELECT product_id
+		FROM (
+			SELECT product_id,
+			COUNT(product_id)  as furniture_counts
+			FROM furniture_apriori
+			GROUP BY product_id
+			HAVING COUNT(product_id) >50) as filtered_list);
+
+-- check for successful import
+SELECT * FROM apriori_analysis LIMIT 10;
+
+-- music segment
+-- add quantity column
+ALTER TABLE music_apriori
+	ADD quantity INT NOT NULL DEFAULT 1;
+
+-- append filtered music data to apriori_analysis table
+-- count threshold = 50 yields xxx,xxx results
+-- count threshold = 100 yields xx,xxx results
+INSERT INTO apriori_analysis (customer_id, product_id, quantity)
+	SELECT customer_id, product_id, quantity
+	FROM music_apriori
+	WHERE product_id IN (
+		SELECT product_id
+		FROM (
+			SELECT product_id,
+			COUNT(product_id)  as music_counts
+			FROM music_apriori
+			GROUP BY product_id
+			HAVING COUNT(product_id) >50) as filtered_list);
+
+-- check for successful import
+SELECT * FROM apriori_analysis LIMIT 10;
+
+-- office_products segment
+-- add quantity column
+ALTER TABLE music_apriori
+	ADD quantity INT NOT NULL DEFAULT 1;
+
+-- append filtered office_products data to apriori_analysis table
+-- count threshold = 50 yields xxx,xxx results
+-- count threshold = 100 yields xx,xxx results
+INSERT INTO apriori_analysis (customer_id, product_id, quantity)
+	SELECT customer_id, product_id, quantity
+	FROM office_products_apriori
+	WHERE product_id IN (
+		SELECT product_id
+		FROM (
+			SELECT product_id,
+			COUNT(product_id)  as office_products_counts
+			FROM office_products_apriori
+			GROUP BY product_id
+			HAVING COUNT(product_id) >50) as filtered_list);
+
+-- check for successful import
+SELECT * FROM apriori_analysis LIMIT 10;
+
