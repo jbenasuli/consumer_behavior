77fd67884c03f25d2e220912bb5c05c1c77617ef
James Benasuli <jamiebenasuli@gmail.com>
2021-05-06 12:26:02 -0400

    update filter queries, add count list

--- dev/database/schemas-final/apriori-analysis-table.sql
+++ dev/database/schemas-final/apriori-analysis-table.sql
@@ -45,7 +45,7 @@ ALTER TABLE apparel_apriori
 -- count threshold = 50 yields 601,375 results
 -- count threshold = 100 yields 314,501 results
 -- count threshold = 200 yields 145,686 results 83038
--- count threshold = 200 yields 83,038 results 
+-- count threshold = 300 yields 83,038 results 
 
 INSERT INTO apriori_analysis (customer_id, product_id, quantity)
 	SELECT customer_id, product_id, quantity
@@ -57,7 +57,7 @@ INSERT INTO apriori_analysis (customer_id, product_id, quantity)
 			COUNT(product_id)  as apparel_counts
 			FROM apparel_apriori
 			GROUP BY product_id
-			HAVING COUNT(product_id) >200) as filtered_list);
+			HAVING COUNT(product_id) >300) as filtered_list);
 
 -- check for successful import
 SELECT * FROM apriori_analysis LIMIT 10;
@@ -68,8 +68,11 @@ ALTER TABLE furniture_apriori
 	ADD quantity INT NOT NULL DEFAULT 1;
 
 -- append filtered furniture data to apriori_analysis table
--- count threshold = 50 yields xxx,xxx results
--- count threshold = 100 yields xx,xxx results
+-- count threshold = 50 yields 299,257 results
+-- count threshold = 100 yields 207,772 results
+-- count threshold = 200 yields 125,441 results
+-- count threshold = 300 yields 89,579 results 
+
 INSERT INTO apriori_analysis (customer_id, product_id, quantity)
 	SELECT customer_id, product_id, quantity
 	FROM furniture_apriori
@@ -80,7 +83,7 @@ INSERT INTO apriori_analysis (customer_id, product_id, quantity)
 			COUNT(product_id)  as furniture_counts
 			FROM furniture_apriori
 			GROUP BY product_id
-			HAVING COUNT(product_id) >50) as filtered_list);
+			HAVING COUNT(product_id) >300) as filtered_list);
 
 -- check for successful import
 SELECT * FROM apriori_analysis LIMIT 10;
@@ -91,8 +94,10 @@ ALTER TABLE music_apriori
 	ADD quantity INT NOT NULL DEFAULT 1;
 
 -- append filtered music data to apriori_analysis table
--- count threshold = 50 yields xxx,xxx results
--- count threshold = 100 yields xx,xxx results
+-- count threshold = 50 yields 490,186 results
+-- count threshold = 100 yields 295,764 results
+-- count threshold = 200 yields 151,669 results
+-- count threshold = 300 yields 91,067 results 
 INSERT INTO apriori_analysis (customer_id, product_id, quantity)
 	SELECT customer_id, product_id, quantity
 	FROM music_apriori
@@ -103,19 +108,30 @@ INSERT INTO apriori_analysis (customer_id, product_id, quantity)
 			COUNT(product_id)  as music_counts
 			FROM music_apriori
 			GROUP BY product_id
-			HAVING COUNT(product_id) >50) as filtered_list);
+			HAVING COUNT(product_id) >300) as filtered_list);
 
 -- check for successful import
 SELECT * FROM apriori_analysis LIMIT 10;
 
 -- office_products segment
 -- add quantity column
-ALTER TABLE music_apriori
+ALTER TABLE office_products
 	ADD quantity INT NOT NULL DEFAULT 1;
 
--- append filtered office_products data to apriori_analysis table
--- count threshold = 50 yields xxx,xxx results
--- count threshold = 100 yields xx,xxx results
+-- append filtered office_products data to apriori_analysis table 132709
+-- count threshold = 50 yields 1,169,618 results
+-- count threshold = 100 yields 889,244 results
+-- count threshold = 200 yields 615,297 results
+-- count threshold = 300 yields 459,558 results
+-- count threshold = 400 yields 359,614 results
+-- count threshold = 500 yields 295,947 results
+-- count threshold = 600 yields 238,108 results
+-- count threshold = 700 yields 204,661 results
+-- count threshold = 800 yields 169,680 results
+-- count threshold = 900 yields 152,761 results
+-- count threshold = 1000 yields 132,709 results
+-- count threshold = 1250 yields 95,172 results
+
 INSERT INTO apriori_analysis (customer_id, product_id, quantity)
 	SELECT customer_id, product_id, quantity
 	FROM office_products_apriori
@@ -137,8 +153,8 @@ ALTER TABLE personal_care_appliances_apriori
 	ADD quantity INT NOT NULL DEFAULT 1;
 
 -- append filtered personal_care_appliances data to apriori_analysis table
--- count threshold = 50 yields xxx,xxx results
--- count threshold = 100 yields xx,xxx results
+-- count threshold = 25 yields 32,545 results
+-- count threshold = 50 yields 26,738 results
 INSERT INTO apriori_analysis (customer_id, product_id, quantity)
 	SELECT customer_id, product_id, quantity
 	FROM personal_care_appliances_apriori
@@ -160,8 +176,13 @@ ALTER TABLE video_games_apriori
 	ADD quantity INT NOT NULL DEFAULT 1;
 
 -- append filtered video_games data to apriori_analysis table
--- count threshold = 50 yields xxx,xxx results
--- count threshold = 100 yields xx,xxx results
+-- count threshold = 50 yields 819,043 results
+-- count threshold = 100 yields 665,048 results
+-- count threshold = 200 yields 488,011 results
+-- count threshold = 300 yields 380,130 results
+-- count threshold = 500 yields 242,321 results
+-- count threshold = 1000 yields 93,885 results
+
 INSERT INTO apriori_analysis (customer_id, product_id, quantity)
 	SELECT customer_id, product_id, quantity
 	FROM video_games_apriori
@@ -183,8 +204,10 @@ ALTER TABLE videos_apriori
 	ADD quantity INT NOT NULL DEFAULT 1;
 
 -- append filtered videos data to apriori_analysis table
--- count threshold = 50 yields xxx,xxx results
--- count threshold = 100 yields xx,xxx results
+-- count threshold = 5 yields 31,403 results
+-- count threshold = 10 yields 14,602 results
+-- count threshold = 25 yields 3,384 results
+-- count threshold = 50 yields 544 results
 INSERT INTO apriori_analysis (customer_id, product_id, quantity)
 	SELECT customer_id, product_id, quantity
 	FROM videos_apriori
@@ -206,8 +229,12 @@ ALTER TABLE watches_apriori
 	ADD quantity INT NOT NULL DEFAULT 1;
 
 -- append filtered watches data to apriori_analysis table
--- count threshold = 50 yields xxx,xxx results
--- count threshold = 100 yields xx,xxx results
+-- count threshold = 50 yields 343,990 results
+-- count threshold = 100 yields 245,557 results
+-- count threshold = 200 yields 158,497 results
+-- count threshold = 300 yields 110,186 results
+-- count threshold = 400 yields 81,482 results
+
 INSERT INTO apriori_analysis (customer_id, product_id, quantity)
 	SELECT customer_id, product_id, quantity
 	FROM watches_apriori
