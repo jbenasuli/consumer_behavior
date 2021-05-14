0f2d1acb5b898e752ad3deaa1829f0cab7f212b2
James Benasuli <jamiebenasuli@gmail.com>
2021-05-14 18:17:02 -0400

    update kmeans chart div

--- deployment/segment-analysis/kmeans-plot.js
+++ deployment/segment-analysis/kmeans-plot.js
@@ -1,13 +1,7 @@
-Plotly.d3.json(jsonUrl, function(err, fig) {
+Plotly.d3.json('deployment/segment-analysis/kmeanschart.json', function(err, fig) {
     // assuming json is formatted as { "data": [/* */], "layout": {/* */} }
   
-    Plotly.plot('graph-id', fig.data, fig.layout);
+    Plotly.plot('graph-kmeans', fig.data, fig.layout);
   });
-
-// var myJson = '{"data","layout"}';
-
-// var figure = JSON.parse(myJson);
-
-// Plotly.newPlot('graph-div', figure.data, figure.layout);
 
 console.log( Plotly.BUILD );
