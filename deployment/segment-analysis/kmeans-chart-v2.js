0a979ab8285997c300efad0f66051fad031acbdd
James Benasuli <jamiebenasuli@gmail.com>
2021-05-14 19:41:03 -0400

    add kmeans-v2 templates

---
+++ deployment/segment-analysis/kmeans-chart-v2.js
@@ -0,0 +1,7 @@
+Plotly.d3.json('deployment/segment-analysis/kmeans-chart-v2.json', function(err, fig) {
+    // assuming json is formatted as { "data": [/* */], "layout": {/* */} }
+  
+    Plotly.plot('graph-kmeans-v2', fig.data, fig.layout);
+  });
+
+console.log( Plotly.BUILD );
