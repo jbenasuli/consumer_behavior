0c43750f35aa12b90b4b914e861892eec0341615
James Benasuli <jamiebenasuli@gmail.com>
2021-05-15 11:16:37 -0400

    rm old kmeans chart/data

--- deployment/segment-analysis/kmeans-plot.js
+++
@@ -1,7 +0,0 @@
-Plotly.d3.json('deployment/segment-analysis/kmeanschart.json', function(err, fig) {
-    // assuming json is formatted as { "data": [/* */], "layout": {/* */} }
-  
-    Plotly.plot('graph-kmeans', fig.data, fig.layout);
-  });
-
-console.log( Plotly.BUILD );
